081d7dd7afeb16b15229051c2114b459c0633528
vicfriedman <victoria.m.friedman@gmail.com>
2016-01-11 17:11:53 -0500

    filled in stars controller

--- app/controllers/stars_controller.rb
+++ app/controllers/stars_controller.rb
@@ -1,4 +1,25 @@
 class StarsController < ApplicationController
 
-  
+  get '/stars/new' do 
+    erb :'/stars/new'
+  end
+
+  post '/stars' do 
+   #code solution here
+  end
+
+  get '/stars/:id' do 
+    @star = Star.find(params[:id])
+    erb :'/stars/show'
+  end
+
+  post '/stars/:id' do 
+    #code solution here
+  end
+
+  get '/stars/:id/edit' do 
+    @star = Star.find(params[:id])
+    erb :'/stars/edit'
+  end
+
 end
